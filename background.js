52d56d0d65aad741aaf184f67e127b5ebce0a19e
saccharide <rtang@email.wm.edu>
2019-05-09 15:54:22 -0400

    some update, will include html2canvas soon

--- background.js
+++ background.js
@@ -1,41 +1,48 @@
-// Script running in the background
-window.onload = function() {
-    //var bkg = chrome.extension.getBackgroundPage();
-    if (window.location.href.includes(".md")){
-
-        html2canvas(document.querySelector("#readme")).then(canvas => {
-            document.body.appendChild(canvas)
-            // var image = canvas.toDataURL("image/png").replace("image/png","image/octet-stream");
-            // image.setAttribute('crossOrigin', 'anonymous');
-            // img.src = url;
-            // window.location.href=image;
-        });
-    }
-    else{
-        chrome.tabs.onActivated.addListener(function(activeInfo) {
-              // how to fetch tab url using activeInfo.tabid
-            chrome.tabs.get(activeInfo.tabId, function(tab){
-                chrome.tabs.executeScript(
-                              tab.id,
-                              {code: 'console.log("Not on a readme page")'}
-                );
-              }); 
-        })
-//        chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
-//            chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+//// Script running in the background
+//window.onload = function() {
+//    //var bkg = chrome.extension.getBackgroundPage();
+//    if (window.location.href.includes(".md")){
+//        chrome.tabs.onActivated.addListener(function(activeInfo) {
+//              // how to fetch tab url using activeInfo.tabid
+//            //   chrome.tabs.get(activeInfo.tabId, function(tab){
+//            //        console.log(tab.url);
+//            //          });
+//            //          }); 
+//            //   })
+//        })
+//        html2canvas(document.querySelector("#readme")).then(canvas => {
+//            document.body.appendChild(canvas)
+//            // var image = canvas.toDataURL("image/png").replace("image/png","image/octet-stream");
+//            // image.setAttribute('crossOrigin', 'anonymous');
+//            // img.src = url;
+//            // window.location.href=image;
+//        });
+//    }
+//    else{
+//        chrome.tabs.onActivated.addListener(function(activeInfo) {
+//              // how to fetch tab url using activeInfo.tabid
+//            chrome.tabs.get(activeInfo.tabId, function(tab){
 //                chrome.tabs.executeScript(
-//                              tabs[0].id,
+//                              tab.id,
 //                              {code: 'console.log("Not on a readme page")'}
 //                );
-//                    
-//            });
-                //code in here will run every time a user goes onto a new tab, so you can insert your scripts into every new tab
-            //}); 
-        //console.log("Not on a readme page");
-//        chrome.tabs.executeScript({
-//                code: 'console.log("Not on a readme page")'
-//            
-//        });
-        }
-}
-
+//              }); 
+//        })
+////        chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
+////            chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+////                chrome.tabs.executeScript(
+////                              tabs[0].id,
+////                              {code: 'console.log("Not on a readme page")'}
+////                );
+////                    
+////            });
+//                //code in here will run every time a user goes onto a new tab, so you can insert your scripts into every new tab
+//            //}); 
+//        //console.log("Not on a readme page");
+////        chrome.tabs.executeScript({
+////                code: 'console.log("Not on a readme page")'
+////            
+////        });
+//        }
+//}
+//
--- content.js
+++ content.js
@@ -41,9 +41,48 @@
 // Version 2.1
 // Instead of waiting for a message being sent by the popup, we will just count the
 //
-console.log("--> Not on readme page");
 
 
+
+
+
+
+
+
+
+//    //var bkg = chrome.extension.getBackgroundPage();
+//if (window.location.href.includes(".md")){
+////    rome.tabs.onActivated.addListener(function(activeInfo) {
+////          // how to fetch tab url using activeInfo.tabid
+////        //   chrome.tabs.get(activeInfo.tabId, function(tab){
+////        //        console.log(tab.url);
+////        //          });
+////        //          }); 
+////        //   })
+////    })
+//    console.log("DEBUG: I am on a README page!");
+//    html2canvas(document.querySelector("#readme")).then(canvas => {
+//        document.body.appendChild(canvas)
+//        // var image = canvas.toDataURL("image/png").replace("image/png","image/octet-stream");
+//        // image.setAttribute('crossOrigin', 'anonymous');
+//        // img.src = url;
+//        // window.location.href=image;
+//    });
+//                
+//
+//}
+//else{
+//    console.log("DEBUG: I am NOT on a README page ...");
+////    chrome.tabs.onActivated.addListener(function(activeInfo) {
+////          // how to fetch tab url using activeInfo.tabid
+////        chrome.tabs.get(activeInfo.tabId, function(tab){
+////            chrome.tabs.executeScript(
+////                          tab.id,
+////                          {code: 'console.log("Not on a readme page")'}
+////            );
+////          }); 
+////    })
+//}
 
 
 
